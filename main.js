67bb494a974ad469395a7c35a1311f529ed152dc
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-05-09 15:43:40 +0300

    train game less

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 const min = 0
 const max = 100
-const attCount = 6
+const attCounter = 5
 
-const getRandomNumber = maxLimit => Math.round(Math.random() * maxLimit)
-const randomNumber = getRandomNumber(max)
-
-const playGame = (minLimit, maxLimit, attsNumber, number) => {
-    alert(`Я загадал число от ${minLimit} до ${maxLimit}. У тебя есть ${attsNumber} попыток`)
-
-    for (i = 1; i <= attCount; i++) {
-        const answer = prompt("Введите свой ответ ниже")
-
-        if (answer > number) {
-            alert("Загаданное число меньше.")
-        } else if (answer < number) {
-            alert("Загаданное число больше.")
-        } else {
-            alert(`Поздравляю . Ты победил с ${i}-й попытки.`)
-            return
-        } 
-    }
-    alert(`У меня для тебя плохие новости. Ты все проиграл. Загаданное число: ${number}. Иди домой неудачник.`)
+function getUserName () {
+    const userName = prompt("Добрый день или вечер. Введите свое имя:")
+    return userName
 }
 
-playGame(min, max, attCount, randomNumber)
+const userName = getUserName() 
+
+const getRandomNumber = (maxLimit) => Math.round(Math.random() * maxLimit)
+const randomNumber = getRandomNumber(max)
+
+const playGame = (minLimit, maxLimit, attsNumber, number, name) => {
+    alert(`Добро пожаловать в игру "Больше или Меньше", ${name}!!! Я загадал число от ${minLimit} до ${maxLimit}. Тебе нужно угадать это число. У тебя есть ${attsNumber} попыток.`)
+
+    for (let i = 1; i <= attCounter; i++) {
+        const answer = prompt("Введи число в поле")
+
+        if (answer > number) {
+            alert(`Загаданное число меньше. У тебя осталось ${attsNumber - i} попыток.`)
+        } else if (answer < number) {
+            alert(`Загаданное число больше. У тебя осталось ${attsNumber - i} попыток.`)
+        } else { alert(`${name}, ты победил с ${i}-й попытки. Загаданное число ${number}.`)
+        return}
+        
+    }
+    alert(`${name}, ты проиграл. Загаданное число ${number}`)
+}
+
+playGame(min, max, attCounter, randomNumber, userName)
